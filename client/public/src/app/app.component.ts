541d8afe32bd24555e80c41676635657cc0c4750
dingzhengfuaa <dingzhengfuaa@gmail.com>
2018-08-16 14:46:26 -0700

    finished main page
    need to implement sockets

--- client/public/src/app/app.component.ts
+++ client/public/src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core'; 
+import { Component, OnInit } from '@angular/core'; 
+import { HttpService } from './http.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title= 'bruh';
+export class AppComponent implements OnInit {
+  stocks = {
+    "DJIA": {
+    },
+    ".IXIC": {
+    },
+    ".INX": {
+    }
+  };
+  price;
+  change;
+  percentChange;
+  constructor(private _httpService: HttpService){}
+
+  ngOnInit(){
+    this.getStockBySymbol("DJIA");
+    this.getStockBySymbol(".IXIC");
+    this.getStockBySymbol(".INX");
+  }
+
+  getStockBySymbol(ticker) {
+    let observable = this._httpService.getStockBySymbol(ticker);
+    observable.subscribe(data => {
+      var date = data["Meta Data"]["3. Last Refreshed"];
+      this.stocks[ticker]["price"] = parseFloat(data["Time Series (Daily)"][date]["5. adjusted close"]).toFixed(2);
+      var open = parseFloat(data["Time Series (Daily)"][date]["1. open"]);
+      this.stocks[ticker]["change"] = (this.stocks[ticker]["price"] - open).toFixed(2);
+      this.stocks[ticker]["percent"] = (100*(this.stocks[ticker]["change"])/open).toFixed(2);
+    });
+  }
 }
--- client/public/src/app/http.service.ts
+++ client/public/src/app/http.service.ts
@@ -6,4 +6,8 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HttpService {
   constructor(private _http: HttpClient){}
+
+  getStockBySymbol(ticker) {
+    return this._http.get(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${ticker}&apikey=Z33FIUU3K8QXGKSD`);
+  }
 }
